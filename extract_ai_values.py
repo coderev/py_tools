33d91fa03cbe5f328b51982f1d5d52c8a6d2303c
enze <mytale@qq.com>
2017-09-13 17:52:31 +0800

    improvements

--- extract_ai_values.py
+++ extract_ai_values.py
@@ -2,6 +2,7 @@ import os
 import re
 import shutil
 print "scanning process ..."
+
 class varEntity(object):
 	"""docstring for varEntity"""
 	#def __init__(self, arg):
@@ -12,10 +13,15 @@ class varEntity(object):
 	evType = "AEIV"
 	evValOLD = 0
 	evValNEW = 0
+	evDelta = 0
+	evAbsDelta = 0
+
+
+re_normal_pattern = re.compile(r'Variable\s\[(\w+-.+)\]\s@\s\((\d+/\d+/\d+/\d+/\d+)\):\s(\w+)\s:\sOLD\s(\d+[\.?|\s?]\d+)\S\sNEW\s(\d+[\.?|\s?]\d+)')
+re_badlength_pattern = re.compile(r'Bad length.+AEIV.+\[(\w+\-\w+)\]\s@\s\((\d+/\d+/\d+/\d+/\d+)\)')
 fileList=[]
 evChanged=[]
-re_normal_pattern = re.compile(r'Variable\s\[(\w+-.+)\]\s@\s\((\d+/\d+/\d+/\d+/\d+)\):\s(\w+)\s:\sOLD\s(\d+[\.?|\s?]\d+)\S\sNEW\s(\d+[\.?|\s?]\d+)')
-re_badlength_pattern = re.compile(r'\[(\w+-.+)\]')
+
 def listFiles(dirPath):
 	cnt = 0
 	for root, dirs,files in os.walk(dirPath):
@@ -25,20 +31,21 @@ def listFiles(dirPath):
 			cnt = cnt + 1
 	print "scanning process is over. " + str(cnt) + " file(s) added."
 	return fileList
+
 def main():
-	#k=tuple([1,2,3])
-	#print k, type(k)
-	#print str(k),type(str(k))
-	#return
-	baseDir = "G:/py_tools/detailed_ai_log"
+	baseDir = "C:\\UnifyIO\\log\\log_OCCR2_20170909110000"
 	fileList = listFiles(baseDir)
+	fileNumber = str(len(fileList))
+	fileCnt = 0
 	for filePath in fileList:
-		all_lines = open(filePath).readlines()
+		print "processing file: " + filePath + " ["+ str(fileCnt+1) +" of " + fileNumber +"]..."
 		cnt = 0
+		prev_ev_changed = len(evChanged)		
+		all_lines = open(filePath).readlines()
+		
 		for line in all_lines:
 			m = re_normal_pattern.search(line)
 			if m:
-				#print m.group(1), m.group(2), m.group(3), m.group(4), m.group(5)
 				evGet = varEntity()
 				evGet.evName = m.group(1) 
 				evGet.evAddr = m.group(2)
@@ -51,13 +58,16 @@ def main():
 					evGet.evValNEW = int(m.group(5)[0:1])				
 				else:
 					evGet.evValNEW = float(m.group(5))
-				#print "name=" + evGet.evName + ", " + evGet.evType + ", old=" + str(evGet.evValOLD) + ", new=" + str(evGet.evValNEW) + ", delta=" + str( evGet.evValNEW - evGet.evValOLD)
+				evGet.evDelta = evGet.evValNEW - evGet.evValOLD
 				evChanged.append(evGet)
 			cnt = cnt + 1
-		for x in xrange(0,len(evChanged)):
-			evGet = evChanged[x]
-			print "name=" + evGet.evName + ", " + evGet.evType + ", old=" + str(evGet.evValOLD) + ", new=" + str(evGet.evValNEW) + ", delta=" + str( evGet.evValNEW - evGet.evValOLD)
-		print "the ev changed in file [" + filePath + "] has been found " + str(len(evChanged)) + " items"
+
+		print "ev changed: " + str(len(evChanged) - prev_ev_changed) + " items, and total got: " + str(len(evChanged)) +" items."
+		fileCnt = fileCnt + 1
+	for x in xrange(0,len(evChanged)):
+		evGet = evChanged[x]
+		print "name=" + evGet.evName + ", " + evGet.evType + ", old=" + str(evGet.evValOLD) + ", new=" + str(evGet.evValNEW) + ", delta=" + str( abs(evGet.evDelta))	
 	return
+
 main()
 print "all done."
